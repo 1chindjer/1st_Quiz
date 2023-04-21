30a426023760c30e9f3695c70175635190f59db8
1chindjer <chindjer@gmail.com>
2023-04-21 21:30:18 +0300

    Создал ввод массива строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите строки. Разделите их запятыми и пробелами, после ввода нажмите Enter:");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+int shortStringsCount = 0;
+    for (int i = 0; i < inputArray.Length; i++){
+        if (inputArray[i].Length <= 3){
+            shortStringsCount++;
+            }
+    }
+// Создаем новый массив для строк длиной не больше 3
